3ff2035b60f33bd427a3594d4206a942ec1101f5
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-20 04:43:38 +0330

    modifying codes

--- src/util/reset-xui.js
+++ src/util/reset-xui.js
@@ -1,33 +1,20 @@
 const { spawn } = require("child_process");
 
 const resetX = async () => {
-  return new Promise((resolve, reject) => {
-    const app = spawn("x-ui");
+  // Spawn the x-ui process
+  const app = spawn("x-ui");
 
-    // Listen for the "close" event of the stdin stream
-    app.stdin.on("close", () => {
-      console.log("stdin close event fired");
-      console.log("app process status:", app.exitCode);
-      app.kill();
-      resolve(200);
-    });
+  // Send "10" to the process's stdin stream
+  app.stdin.write("10\n");
 
-    // Write "10" to the stdin stream
-    app.stdin.write("10");
-
-    // Handle errors
-    app.on("error", (err) => {
-      console.log(`cant restart x-ui cause ${err}`);
-      reject(err);
-    });
-
-    // Listen for the "exit" event of the child process
-    app.on("exit", (code, signal) => {
-      console.log("app exit event fired");
-      console.log("exit code:", code);
-      console.log("signal:", signal);
+  // Wait for the process to exit and resolve with its status code
+  const status = await new Promise((resolve) => {
+    app.on("exit", (code) => {
+      resolve(code);
     });
   });
+
+  return status;
 };
 
 module.exports = resetX;
