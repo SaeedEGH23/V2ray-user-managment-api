f1c79e99e56646c720c4068631f719800ffa4cd0
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-22 01:48:11 +0330

    make traffic show better

--- src/util/showtraffic.js
+++ src/util/showtraffic.js
@@ -1,7 +1,11 @@
 // return traffic in gb
 const showTraffic = (value) => {
-  if (value > 0) return value / 1024 / 1024 / 1024;
-  else return 0;
+  if (value > 0) {
+    let MB = Math.trunc(value / 1024 / 1024);
+    let GB = Math.trunc(MB / 1024);
+    MB = MB % 1024;
+    return GB > 0 ? `${GB}.${Math.trunc(MB / 10)}GB` : `${MB}MB`;
+  } else return 0;
 };
 
 module.exports = showTraffic;
