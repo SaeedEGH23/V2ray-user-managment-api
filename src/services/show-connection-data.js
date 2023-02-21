e212d83ba286a6d90a5c0e7c4efe4a514a3201c5
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-22 03:18:38 +0330

    Add new service to return connection data

--- src/services/show-connection-data.js
+++ src/services/show-connection-data.js
@@ -1,7 +1,27 @@
 const Inbound = require("../model/inbounds.js");
+const showTraffic = require("../util/showtraffic.js");
+const showremaintime = require("../util/showremaintime.js");
 
-async function check(idNum) {
-  let x = await Inbound.searchId(1);
-  console.log("did it --------", x);
-}
-check;
+const getConnectionData = async (remark) => {
+  try {
+    let data = await Inbound.findByRemark(remark.cname);
+    if (data == undefined)
+      return `Connection with name ${remark.cname} does not exist!`;
+    let traficRemain =
+      data.total == 0
+        ? "Unlimited"
+        : showTraffic(data.total - (data.up + data.down));
+    let timeRemain =
+      data.expiry_time == 0 ? "Unlimited" : showremaintime(data.expiry_time);
+    let dataInbound = {
+      username: `${data.remark}`,
+      RemainTime: `${timeRemain}`,
+      RemainTraffic: `${traficRemain}`,
+    };
+    return dataInbound;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = getConnectionData;
