efaac8e29994a71d56121cd010e892b6a809a7cc
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-24 12:02:27 +0330

    add error generator for limitation server

--- src/services/insert-connection.js
+++ src/services/insert-connection.js
@@ -7,16 +7,32 @@ const { resolve } = require("path");
 
 require("dotenv").config();
 
+class LimitationError extends Error {
+  constructor(message) {
+    super(message);
+    this.message = "Server reach limitation";
+  }
+}
+
 const VPNdomain = process.env.VPN_DOMAIN;
 const certPath = process.env.CERT_PATH || "/root/cert.crt";
 const privatePath = process.env.PRIVATE_PATH || "/root/private.key";
+const totalLimitation = process.env.GLOBAL_LIMITATION || 0;
+const enableLimitation = process.env.ENLABLED_LIMITATION || 0;
 
 let connectionPortNumber, password, traffic, remark, protocol, period;
 
 const createInsertRequest = async (data) => {
-  // console.log(VPNdomain, certPath, privatePath);
+  const totalTop = await Inbound.getTotalInbounds();
+  const enableTop = await Inbound.getTotalEnableInbounds();
+  console.log(`service insert: ${totalLimitation} total ${totalTop}`);
+  console.log(`service insert: ${enableLimitation} total ${enableTop}`);
+  if (totalLimitation != 0 || enableLimitation != 0) {
+    if (totalTop > totalLimitation) throw new LimitationError();
+    if (enableTop > enableLimitation) throw new LimitationError();
+  }
+
   password = crypto.randomBytes(5).toString("hex");
-  // console.log("check");
 
   connectionPortNumber = await portGenerator();
   console.log(connectionPortNumber);
