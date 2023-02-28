0d2c897b1a92b6bef4f4f39d1f0aecbb5210b634
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-28 21:32:43 +0330

    Set if period 0 return 0

--- src/util/timesetting.js
+++ src/util/timesetting.js
@@ -1,6 +1,7 @@
 // It's return month's in int set it for expire time from now with functionCall([month number] || default 1)
 
-const timeSet = function (month = 1) {
+const timeSet = function (month) {
+  if (month == 0) return 0;
   let timestamp = Date.now();
   timestamp += month * (30 * 24 * 60 * 60 * 1000);
   let d = new Date();
