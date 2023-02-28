e4bdab807d46a2e688ed1fdb781601a8d754cf27
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-28 17:56:05 +0330

    debug : remove async behind of func

--- src/util/timesetting.js
+++ src/util/timesetting.js
@@ -1,6 +1,6 @@
 // It's return month's in int set it for expire time from now with functionCall([month number] || default 1)
 
-const timeSet = async function (month = 1) {
+const timeSet = function (month = 1) {
   let timestamp = Date.now();
   timestamp += month * (30 * 24 * 60 * 60 * 1000);
   let d = new Date();
