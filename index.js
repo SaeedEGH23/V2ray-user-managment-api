f7d97d26f27be69d5d07193a1420e07d1c6b85e2
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-03-04 17:33:51 +0330

    cahngestructure

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const checkAuthToken = require("./src/services/check-auth-token.js");
 const connections = require("./src/controller/connections.js");
 const validate = require("./src/services/validator-income-req.js");
+const routes = require("./src/routes/v1");
 require("dotenv").config();
 const port = process.env.LISTEN_PORT;
 
@@ -12,22 +13,23 @@ app.use("*", checkAuthToken);
 
 app.use(bodyParser.json());
 
-app.use("/createUser", validate.middleInsValid);
-app.use("/remainCheck", validate.middleGetDataValid);
-app.use("/updateAccount", validate.middleUpdateAccount);
+// app.use("/createUser", validate.middleInsValid);
+// app.use("/remainCheck", validate.middleGetDataValid);
+// app.use("/updateAccount", validate.middleUpdateAccount);
 
 // configure routes
+app.use("/v1", routes);
 
-app.post("/createUser", async (req, res) => {
-  // route to controller
-  const data = req.body;
-  try {
-    const userLink = await connections.createConnection(data);
-    res.status(200).send(userLink);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
+// app.post("/createUser", async (req, res) => {
+//   // route to controller
+//   const data = req.body;
+//   try {
+//     const userLink = await connections.createConnection(data);
+//     res.status(200).send(userLink);
+//   } catch (err) {
+//     res.status(500).send(err.message);
+//   }
+// });
 
 app.post("/updateAccount", async (req, res) => {
   // route to controller
