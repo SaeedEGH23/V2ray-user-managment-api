075880f39b30381f590bda29f7703bac0fc7aa26
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-03-06 19:20:50 +0330

    change middlware check

--- src/routes/v1/creatUser.route.js
+++ src/routes/v1/creatUser.route.js
@@ -5,10 +5,10 @@ const connectionValidation = require("../../validations/connection.validation");
 const express = require("express");
 const router = express.Router();
 
-// router.post(
-//   "/createUser",
-//   validate(connectionValidation.createSchema),
-//   controller.createConnection
-// );
+router.post(
+  "/createUser",
+  validate(connectionValidation.createSchema),
+  controller.createConnection
+);
 
 module.exports = router;
