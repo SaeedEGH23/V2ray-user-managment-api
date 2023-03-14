534da34d9d0862bbd3d6855df8b58f61d7648b56
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-03-14 18:08:35 +0330

    refactore firewall allow to promise function

--- src/util/firewall-allow.js
+++ src/util/firewall-allow.js
@@ -1,44 +1,89 @@
+const { rejects } = require("assert");
 const { exec } = require("child_process");
+const { resolve } = require("path");
 
 // Check if UFW is enabled
+// const allower = (port) => {
+//   exec("ufw status", (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error checking UFW status: ${error}`);
+//       return;
+//     }
+
+//     if (stderr) {
+//       console.error(`UFW status error: ${stderr}`);
+//       return;
+//     }
+
+//     // Check if UFW is active/enabled
+//     if (stdout.includes("active")) {
+//       console.log("UFW is enabled");
+
+//       // Allow incoming traffic on the specified port
+//       exec(`ufw allow ${port}`, (error, stdout, stderr) => {
+//         if (error) {
+//           console.error(`Error allowing port ${port}: ${error}`);
+//           return;
+//         }
+
+//         if (stderr) {
+//           console.error(`Error allowing port ${port}: ${stderr}`);
+//           return;
+//         }
+
+//         if (stdout) {
+//           console.log(stdout);
+//           return;
+//         }
+
+//         console.log(`Port ${port} allowed`);
+//       });
+//     } else {
+//       console.log("UFW is not enabled");
+//     }
+//   });
+// };
+
 const allower = (port) => {
-  exec("ufw status", (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error checking UFW status: ${error}`);
-      return;
-    }
+  return new Promise((resolve, rejects) => {
+    exec("ufw status", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error checking UFW status: ${error}`);
+        return;
+      }
 
-    if (stderr) {
-      console.error(`UFW status error: ${stderr}`);
-      return;
-    }
+      if (stderr) {
+        console.error(`UFW status error: ${stderr}`);
+        return;
+      }
 
-    // Check if UFW is active/enabled
-    if (stdout.includes("active")) {
-      console.log("UFW is enabled");
+      // Check if UFW is active/enabled
+      if (stdout.includes("active")) {
+        console.log("UFW is enabled");
 
-      // Allow incoming traffic on the specified port
-      exec(`ufw allow ${port}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error allowing port ${port}: ${error}`);
-          return;
-        }
+        // Allow incoming traffic on the specified port
+        exec(`ufw allow ${port}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error allowing port ${port}: ${error}`);
+            return;
+          }
 
-        if (stderr) {
-          console.error(`Error allowing port ${port}: ${stderr}`);
-          return;
-        }
+          if (stderr) {
+            console.error(`Error allowing port ${port}: ${stderr}`);
+            return;
+          }
 
-        if (stdout) {
-          console.log(stdout);
-          return;
-        }
+          if (stdout) {
+            console.log(stdout);
+            return;
+          }
 
-        console.log(`Port ${port} allowed`);
-      });
-    } else {
-      console.log("UFW is not enabled");
-    }
+          console.log(`Port ${port} allowed`);
+        });
+      } else {
+        console.log("UFW is not enabled");
+      }
+    });
   });
 };
 
