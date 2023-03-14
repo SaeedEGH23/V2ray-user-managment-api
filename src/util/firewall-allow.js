b2672fee0041b5fcd32ac3f3201056d0e49c9d41
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-03-14 18:13:43 +0330

    delete usless codes

--- src/util/firewall-allow.js
+++ src/util/firewall-allow.js
@@ -3,46 +3,6 @@ const { exec } = require("child_process");
 const { resolve } = require("path");
 
 // Check if UFW is enabled
-// const allower = (port) => {
-//   exec("ufw status", (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error checking UFW status: ${error}`);
-//       return;
-//     }
-
-//     if (stderr) {
-//       console.error(`UFW status error: ${stderr}`);
-//       return;
-//     }
-
-//     // Check if UFW is active/enabled
-//     if (stdout.includes("active")) {
-//       console.log("UFW is enabled");
-
-//       // Allow incoming traffic on the specified port
-//       exec(`ufw allow ${port}`, (error, stdout, stderr) => {
-//         if (error) {
-//           console.error(`Error allowing port ${port}: ${error}`);
-//           return;
-//         }
-
-//         if (stderr) {
-//           console.error(`Error allowing port ${port}: ${stderr}`);
-//           return;
-//         }
-
-//         if (stdout) {
-//           console.log(stdout);
-//           return;
-//         }
-
-//         console.log(`Port ${port} allowed`);
-//       });
-//     } else {
-//       console.log("UFW is not enabled");
-//     }
-//   });
-// };
 
 const allower = (port) => {
   return new Promise((resolve, rejects) => {
