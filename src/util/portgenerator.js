89664172ae6797f0866a6c06c0800c52850e7c03
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-03-18 20:03:23 +0330

    add check for ignore ports

--- src/util/portgenerator.js
+++ src/util/portgenerator.js
@@ -1,6 +1,10 @@
 const { findByPort } = require("../model/inbounds.js");
 const { exec } = require("child_process");
 const e = require("express");
+const { resolve } = require("path");
+const { rejects } = require("assert");
+const { stat } = require("fs");
+const ignores = process.env.IGNOR_PORTS.split(",") || [""];
 require("dotenv").config();
 
 const minPort = parseInt(process.env.MIN_PORT);
@@ -18,13 +22,28 @@ async function checkFree(newPort) {
   });
 }
 
+async function checkIgnores(newPort) {
+  return new Promise((resolve, rejects) => {
+    let status = true;
+    let ignore = [];
+    for (ignore of ignores) {
+      if (newPort === Number(ignore)) {
+        status = false;
+      }
+    }
+    console.log(`this is port generator: status${status}`);
+    status ? resolve(status) : rejects(status);
+  });
+}
+
 const portGenerator = async function report() {
   var random_int =
     Math.floor(Math.random() * (maxPort - minPort + 1)) + minPort;
   let checkBusyDb = await findByPort(random_int);
   let checkBusySh = await checkFree(random_int);
+  let checkIgnoration = await checkIgnores(random_int);
   console.log(checkBusySh, checkBusyDb);
-  if (checkBusyDb == undefined && checkBusySh == undefined) {
+  if (checkBusyDb == undefined && checkBusySh == undefined && checkIgnoration) {
     return random_int;
   } else {
     report();
